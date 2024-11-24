a82b68e3a68ede84fab065dcff0195fc9d05411f
aymenbouhoula <aymenbouhoula0@gmail.com>
2024-11-24 21:21:47 +0100

    backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -41,6 +41,8 @@ async function creationProduct(req, res) {
 
 async function updateProduct(req, res) {}
 
+
+//Delete product
 async function deleteProduct(req, res) {
     try {
         const {id} = req.params
--- utils/validattion.js
+++ utils/validattion.js
@@ -2,12 +2,12 @@ const Joi = require("joi");
 
 function validationProduct(product) {
   const shema = Joi.object({
-    name: Joi.string().max(20).required(),
-    price: Joi.number().max(20).required(),
-    brand: Joi.string().max(20).required(),
-    imageURL: Joi.string().max(20).required(),
+    name: Joi.string().max(200).required(),
+    price: Joi.number().max(200).required(),
+    brand: Joi.string().max(200).required(),
+    imageURL: Joi.string().max(200).required(),
     isAvailable: Joi.boolean().required(),
-    quantityInStore: Joi.number().max(20).required(),
+    quantityInStore: Joi.number().max(200).required(),
   });
 
   return shema.validate(product);
